c823c9dbf936b2017740b1f6698b650f39187116
Pasit Sri-intarasut <ong.pasit@gmail.com>
2023-06-20 16:04:09 +0700

    fix code to use in lambda

--- lambda_handler.py
+++ lambda_handler.py
@@ -4,27 +4,31 @@ import json
 data = pd.read_csv('data/hs.csv')
 section = pd.read_csv('data/sections.csv')
 def handler(event, context):
-    # Parse the event payload
-    payload = event['body']
-    
-    # Check if the 'handler' key is present in the payload
-    if 'handler' in payload:
-        # Extract the handler function name from the payload
-        handler_function_name = payload['handler']
+
+    try:
+        handler_function_name = json.loads(event['body'])['handler']
         
         # Dynamically invoke the corresponding handler function
         if handler_function_name == 'get_hs_code':
-            return handler_get_hscode(payload, context)
-    
-    # Default behavior if 'handler' key is not present or unknown handler name
-    return {
-        'statusCode': 400,
-        'body': 'Invalid or missing handler name in payload'
-    }
+            return handler_get_hscode(event, context)
+        else:
+            return {
+                'statusCode': 400,
+                'body': 'Invalid or missing handler name in payload'
+            }
+
+    except:
+        return {
+            'statusCode': 400,
+            'body': 'Invalid or missing handler name in payload'
+        }
 
 def handler_get_hscode(event, context):
     data = pd.read_csv('data/hs.csv')
-    return data.to_dict(orient='records')
+    return {
+        'body': data.to_json(orient='records'),
+        'statusCode': 200
+    }
 # print(data.head())
 # print(section.head())
 # print(data[data['section'] == 'I'].tail())
